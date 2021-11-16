ce02329eae7ffb2a1140d09de78142096212f884
Joowan Kim <rlawndhks217@gmail.com>
2021-11-16 19:31:55 +0900

    Edit: configs
    - `secret.py` 작성

--- run_cron.py
+++ run_cron.py
@@ -8,6 +8,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import cron
 import tasks
 from src.configs.database import Base, engine
+from src.configs.redis_config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
 
 logging.basicConfig(
     format="[%(asctime)s] [PID %(process)d] [%(threadName)s] [%(name)s] [%(levelname)s] %(message)s",
@@ -22,18 +23,14 @@ def main():
 
     jobstores = {
         'default': RedisJobStore(
-            host='localhost',
-            port=6379,
-            password="redispassword"
+            host=REDIS_HOST,
+            port=REDIS_PORT,
+            password=REDIS_PASSWORD
         )
     }
 
     scheduler = BlockingScheduler(jobstores=jobstores)
-    print(cron.JOBS)
     for trigger, module_path, func_name in cron.JOBS:
-        print(trigger)
-        print(module_path)
-        print(func_name)
         job_path = f"{module_path}:{func_name}.send"
         job_name = f"{module_path}.{func_name}"
         scheduler.add_job(job_path, trigger=trigger, name=job_name)
--- tasks.py
+++ tasks.py
@@ -1,24 +1,17 @@
 import dramatiq
 from dramatiq.brokers.redis import RedisBroker
 
-from datetime import datetime
-
 from cron import cron
 from src.configs.data_source_configs import END_POINT, AUTHORIZATION, SERVICE_KEY
 from src.configs.database import SessionLocal
+from src.configs.redis_config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
 from src.trials.application.services import TrialBatchService
 from src.trials.application.unit_of_work import SqlTrialUnitOfWork
 from src.trials.exceptions import UnauthorizedAuthenticationKeyException, OpenAPIServerErrorException
 from src.trials.infra.data_source import RestTrialDataSource
 
-redis_broker = RedisBroker(host="localhost", port="6379", password="redispassword")
+redis_broker = RedisBroker(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 dramatiq.set_broker(redis_broker)
-
-
-@cron("* * * * *")
-@dramatiq.actor
-def print_current_datetime():
-    print(datetime.now())
 
 
 @cron("* * * * *")
